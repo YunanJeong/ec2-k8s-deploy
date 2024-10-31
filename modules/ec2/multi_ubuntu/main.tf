c4721d5a73281d35a2d582a6ad145f72d774c9e7
yunanjeong <njhgle@gmail.com>
2024-10-31 18:17:19 +0900

    Resolve #13

--- modules/ec2/multi_ubuntu/main.tf
+++ modules/ec2/multi_ubuntu/main.tf
@@ -1,23 +1,33 @@
 ######################################################################
 # Set Up Security Groups
 ######################################################################
-resource "aws_security_group" "allows_basic"{
+resource "aws_security_group" "allows_basic" {
   name = "allows_basic_${var.tags.Name}"
-  ingress{
+  tags = var.tags
+
+  ingress {
     description = "allows all inbounds from my workspace"
-    protocol  = "-1"
+    protocol    = "-1"
     cidr_blocks = var.work_cidr_blocks
-    from_port = 0
-    to_port   = 0
+    from_port   = 0
+    to_port     = 0
   }
-  egress{ # 인스턴스에서 외부로 나가는 request 모두 허용. 이를 없애면 유사 IDC환경 테스트 가능.
+  ingress {
+    description = "between ${var.tags.Name}(private ip)"
+    protocol    = "-1"
+    self        = true  # 보안그룹 셀프 참조. 보안그룹이 등록된 인스턴스끼리 private ip 통신 허용
+    from_port   = 0
+    to_port     = 0
+  }
+  egress { # 인스턴스에서 외부로 나가는 request 모두 허용. 이를 없애면 유사 IDC환경 테스트 가능.
     description = "allows all outbound (apt, ping, ...)"
-    protocol  = "-1"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    from_port = 0
-    to_port   = 0
+    from_port   = 0
+    to_port     = 0
   }
 }
+
 
 ######################################################################
 # Server Instance
@@ -27,64 +37,81 @@ locals {
   valid_subnet_id_list = length(var.subnet_id_list) > 0 ? var.subnet_id_list : [for i in range(var.node_count) : ""]
 }
 resource "aws_instance" "server" {
-  count = var.node_count
-  ami             = var.ami
-  instance_type   = var.instance_type
+  count         = var.node_count
+  ami           = var.ami
+  instance_type = var.instance_type
   # merge에서 나열 순서대로 Update된다.
-  tags            = merge(
-                      var.tags,
-                      { Name="${var.tags.Name}-${count.index}" },
-                    )
+  tags = merge(
+    var.tags,
+    { Name = "${var.tags.Name}-${count.index}" },
+  )
 
-  key_name        = var.key_name
-  subnet_id       = local.valid_subnet_id_list[count.index]
-  vpc_security_group_ids = [        # subnet 지정시 보안그룹은 name이 아닌 id로 등록
+  key_name  = var.key_name
+  subnet_id = local.valid_subnet_id_list[count.index]
+  vpc_security_group_ids = [ # subnet 지정시 보안그룹은 name이 아닌 id로 등록
     aws_security_group.allows_basic.id,
   ]
-  ebs_optimized = true              # EBS최적화 (인스턴스와 볼륨 간 I/O속도 개선)  # 인스턴스타입 따라 비활성화될 수 있음
-  root_block_device{
+  ebs_optimized = true # EBS최적화 (인스턴스와 볼륨 간 I/O속도 개선)  # 인스턴스타입 따라 비활성화될 수 있음
+  root_block_device {
     volume_size = var.volume_size
     # delete_on_termination = true  # default
   }
   # ebs_block_device {}             # 추가 볼륨 or 스냅샷 가져올 때 사용
 
+
+}
+
+# ######################################################################
+# # Public IP로 상호통신 허용 (기존 보안그룹 활용)
+# ######################################################################
+
+# resource "aws_security_group_rule" "self_ingress_public" {
+#   description              = "between ${var.tags.Name}(public ip)"
+#   type                     = "ingress"
+#   from_port                = 0
+#   to_port                  = 0
+#   protocol                 = "-1"
+#   security_group_id        = aws_security_group.allows_basic.id
   
-}
+#   # source_security_group_id = aws_security_group.allows_basic.id
+#   cidr_blocks = concat(
+#     [for ip in aws_instance.server[*].public_ip : replace(ip, ip, "${ip}/32")]
+#   )
+# }
 
-######################################################################
-# Mutual Security Groups between Multiple Instances
-######################################################################
-# 인스턴스 간 보안그룹 생성
-resource "aws_security_group" "allows_mutual"{
-    name = "allows_mutual_${var.tags.Name}"
-    ingress{
-        from_port   = 0
-        to_port     = 0
-        description = "node to node"
-        protocol    = "-1"
+# ######################################################################
+# # Public IP로 상호통신 허용 (신규 보안그룹 생성)
+# ######################################################################
+# resource "aws_security_group" "allows_mutual" {
+#   name = "allows_mutual_${var.tags.Name}"
+#   tags = var.tags
+#   ingress {
+#     from_port   = 0
+#     to_port     = 0
+#     description = "between ${var.tags.Name}(public ip)"
+#     protocol    = "-1"
 
-        # 할당된 인스턴스 ip에 문자열 '/32'를 붙이고 리스트로 반환
-        cidr_blocks = concat(
-          [ for ip in aws_instance.server[*].public_ip: replace(ip,ip,"${ip}/32") ],
-          [ for ip in aws_instance.server[*].private_ip: replace(ip,ip,"${ip}/32") ]
-        )
-    }
-}
-# 인스턴스 간 보안그룹을 각각 인스턴스에 등록
-data "aws_instance" "created_node" {
-    count       = var.node_count  # 반복문 효과
-    instance_id = aws_instance.server[count.index].id  # module's output
-}
-resource "aws_network_interface_sg_attachment" "sg_attach" {
-    count                = var.node_count  # 반복문 효과
-    security_group_id    = aws_security_group.allows_mutual.id
-    network_interface_id = data.aws_instance.created_node[count.index].network_interface_id
-}
+#     # 할당된 인스턴스 ip에 문자열 '/32'를 붙이고 리스트로 반환
+#     cidr_blocks = concat(
+#       [for ip in aws_instance.server[*].public_ip : replace(ip, ip, "${ip}/32")]
+#     )
+#   }
+# }
+# # 인스턴스 간 보안그룹을 각각 인스턴스에 등록
+# data "aws_instance" "created_node" {
+#   count       = var.node_count                      # 반복문 효과
+#   instance_id = aws_instance.server[count.index].id # module's output
+# }
+# resource "aws_network_interface_sg_attachment" "sg_attach" {
+#   count                = var.node_count # 반복문 효과
+#   security_group_id    = aws_security_group.allows_mutual.id
+#   network_interface_id = data.aws_instance.created_node[count.index].network_interface_id
+# }
 
 ######################################################################
 # Server Commands (초기 구성)
 ######################################################################
-resource "null_resource" "server_remote"{
+resource "null_resource" "server_remote" {
   # remote-exec를 위한 ssh connection 셋업
   count = var.node_count
   connection {
